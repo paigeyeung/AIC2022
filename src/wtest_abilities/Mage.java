77539a12e90e5aae3eb2f6465415ff10479c2fcc
WilliamSongLiu <williamsongliu@gmail.com>
2022-07-19 22:45:21 -0700

    mage try swap shrines

--- src/wtest_abilities/Mage.java
+++ src/wtest_abilities/Mage.java
@@ -27,33 +27,45 @@ public class Mage extends AllyUnit {
             return;
         }
 
-        if(communication.cornerTrackingStatus != 2) {
-            return;
-        }
-
-        int distanceSquaredToEnemy = selfLocation.distanceSquared(communication.enemyBaseLocation);
-        boolean inRangeOfEnemy = distanceSquaredToEnemy < uc.getType().getStat(UnitStat.VISION_RANGE);
-
-        uc.println("action " + action + ", inRangeOfEnemy " + inRangeOfEnemy);
-        if(action == 0) {
-            if(uc.canUseFirstAbility(communication.enemyBaseLocation)) {
-                uc.println("using ability");
-                uc.useFirstAbility(communication.enemyBaseLocation);
-                communication.uploadEnemyBaseLocation(selfLocation);
-
-                action = 1;
-            }
-
-            if(!inRangeOfEnemy) {
-                tryAdjMoves(selfLocation.directionTo(communication.enemyBaseLocation));
+        ShrineInfo[] shrines = uc.senseShrines((int)uc.getType().getStat(UnitStat.VISION_RANGE));
+        for(ShrineInfo shrine : shrines) {
+            uc.println("shrine " + shrine.getLocation());
+            if(uc.canUseFirstAbility(shrine.getLocation())) {
+                uc.println("yes");
+                uc.useFirstAbility(shrine.getLocation());
+                return;
             }
         }
-        else {
-            tryAdjMoves(selfLocation.directionTo(communication.allyBaseLocation));
 
-            if(!inRangeOfEnemy && uc.getInfo().getCurrentAbilityICooldown() == 0) {
-                action = 0;
-            }
-        }
+
+
+//        if(communication.cornerTrackingStatus != 2) {
+//            return;
+//        }
+//
+//        int distanceSquaredToEnemy = selfLocation.distanceSquared(communication.enemyBaseLocation);
+//        boolean inRangeOfEnemy = distanceSquaredToEnemy < uc.getType().getStat(UnitStat.VISION_RANGE);
+//
+//        uc.println("action " + action + ", inRangeOfEnemy " + inRangeOfEnemy);
+//        if(action == 0) {
+//            if(uc.canUseFirstAbility(communication.enemyBaseLocation)) {
+//                uc.println("using ability");
+//                uc.useFirstAbility(communication.enemyBaseLocation);
+//                communication.uploadEnemyBaseLocation(selfLocation);
+//
+//                action = 1;
+//            }
+//
+//            if(!inRangeOfEnemy) {
+//                tryAdjMoves(selfLocation.directionTo(communication.enemyBaseLocation));
+//            }
+//        }
+//        else {
+//            tryAdjMoves(selfLocation.directionTo(communication.allyBaseLocation));
+//
+//            if(!inRangeOfEnemy && uc.getInfo().getCurrentAbilityICooldown() == 0) {
+//                action = 0;
+//            }
+//        }
     }
 }
